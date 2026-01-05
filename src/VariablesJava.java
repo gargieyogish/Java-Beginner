b146ede54084f858673a4137951af9bf3567f1de
gargieyogish <gargieyogish@gmail.com>
2026-01-05 20:49:03 +0530

    Java variables

---
+++ src/VariablesJava.java
@@ -0,0 +1,19 @@
+public class VariablesJava {
+    public static void main(String[] args){
+        int age = 20;                 // whole number
+        float heightInCm = 165.5f;    // decimal value
+        double salary = 45550.75;     // large decimal values
+
+        char grade = 'C';             // single character
+        boolean isEligible = false;    // true/false condition
+
+        long phoneNo = 1400000L; // very large numbers
+
+        short year = 2026;            // small-range integer
+        byte rating = 5;              // very small range (e.g., 1–5)
+
+        String name = "Taylor Swift";         // text
+        System.out.println(age);
+
+    }
+}
