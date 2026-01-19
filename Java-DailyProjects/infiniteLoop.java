2e1ea12f569c759c39fcae0ae64655b88db91320
gargieyogish <gargieyogish@gmail.com>
2026-01-19 21:05:04 +0530

    InfiniteLoop-Java

---
+++ Java-DailyProjects/infiniteLoop.java
@@ -0,0 +1,8 @@
+public class infiniteLoop {
+    public static void main(String[]args){
+        do {
+            System.out.println("This loop will run forever");
+        } while (true);
+
+    }
+}
