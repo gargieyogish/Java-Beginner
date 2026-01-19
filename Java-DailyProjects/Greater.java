38d5e736b3c8302ef6abb863158bd972f3f47e2b
gargieyogish <gargieyogish@gmail.com>
2026-01-19 20:57:48 +0530

    GreaterNumber-Java

---
+++ Java-DailyProjects/Greater.java
@@ -0,0 +1,10 @@
+public class Greater {
+   public static int Greater(int a, int b){
+        return (a >b)?a :b;
+    }
+    public static void main(String[] args){
+        int result = Greater(5, 9);
+        System.out.println(result); // 9
+
+    }
+}
